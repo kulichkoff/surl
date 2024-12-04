693a9400c5c631a1fa2363762eec79d9d041d749
Daniel Kulichkov <danchick03@gmail.com>
2024-12-04 20:15:24 +0300

    fix: fix router path params

--- internal/links/router.go
+++ internal/links/router.go
@@ -4,6 +4,6 @@ import "github.com/labstack/echo/v4"
 
 func DefineRoutes(e *echo.Echo) {
 	group := e.Group("/links")
-	group.POST("", handleCreate)
-	group.GET(":slug", handleFindOne)
+	group.POST("/", handleCreate)
+	group.GET("/:slug", handleFindOne)
 }
